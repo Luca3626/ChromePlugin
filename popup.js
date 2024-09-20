c7d2d3158816b170d81d304c879ed46fda838577
Luca <didomenicoluca8@gmail.com>
2024-09-21 01:13:37 +0200

    Aggiunta manifest.json e funzioni in popup.js

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ const all_tabs = document.getElementById("all_tabs");
 const fileInput = document.getElementById("fileInput");
 var textFile = null;
 
+console.log('ciao');
 chrome.tabs.query({windowID: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
     tabs.foreach((tab, index) => {
         const tabItem = document.createElement('div');
@@ -20,5 +21,63 @@ chrome.tabs.query({windowID: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
         label.htmlFor = `tab${index}`;
         label.className = 'tab-label';
         label.appendChild(document.createTextNode(tab.title));
+
+        tabItem.appendChild(checkbox);
+        tabItem.appendChild(label);
+        all_tabs.appendChild(tabItem);
+        all_tabs.appendChild(document.createElement('hr'));
     })
 });
+
+function downloadTabs(tabs, file_name){
+    let text = tabs.map(tab => tab.url).join('\n')
+  
+    let data = new Blob([text], {type: 'text/plain'});
+  
+    if (data !== null) {
+      window.URL.revokeObjectURL(data);
+    }
+  
+    textFile = window.URL.createObjectURL(data);
+  
+    const options = {
+      url: textFile,
+      filename: `${file_name}.txt`,
+  
+    };
+  
+    chrome.downloads.download(options, (downloadId) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      } else {
+        console.log('Download initiated with ID:', downloadId);
+      }
+    });
+  }
+
+  
+  function readFileToArray(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+  
+      reader.onload = (event) => {
+        const contents = event.target.result;
+        const linesArray = contents.split('\n');
+        resolve(linesArray);
+      };
+  
+      reader.onerror = (error) => {
+        reject(error);
+      };
+  
+      reader.readAsText(file);
+    });
+  }
+
+  function dump(){
+    const file_name = document.getElementById("file_name")
+    const checkedItems = Array.from(document.querySelectorAll('input[name="tabs"]:checked'))
+    .map(checkbox => JSON.parse(checkbox.value));
+  
+    downloadTabs(checkedItems, file_name.value)
+  }
